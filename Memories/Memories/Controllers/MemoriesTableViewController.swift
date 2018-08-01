2e1479e8953820be0389e7ca663e2c389057509b
David Doswell <davidoliverdoswell@icloud.com>
2018-08-01 16:07:04 -0700

    project completed with go further implemented - not final

--- Memories/Memories/Controllers/MemoriesTableViewController.swift
+++ Memories/Memories/Controllers/MemoriesTableViewController.swift
@@ -9,74 +9,50 @@
 import UIKit
 
 class MemoriesTableViewController: UITableViewController {
+    
+    let memoryController = MemoryController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        memoryController.loadFromPersistentStore()
+    }
 
     // data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return memoryController.memories.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
-        // Configure the cell...
-
+        let memory = memoryController.memories[indexPath.row]
+        cell.textLabel?.text = memory.title
+        cell.detailTextLabel?.text = memory.bodyText
+        cell.imageView?.image = UIImage(data: memory.imageData)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let memory = memoryController.memories[indexPath.row]
+            memoryController.deleteAMemory(memory: memory)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let vc = segue.destination as? MemoryDetailViewController {
+            vc.memoryController = memoryController
+        }
     }
-    */
 
 }
--- Memories/Memories/Controllers/MemoryDetailViewController.swift
+++ Memories/Memories/Controllers/MemoryDetailViewController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import Photos
 
-class MemoryDetailViewController: UIViewController {
+private let addMemory = "Add Memory"
+private let editMemory = "Edit Memory"
+
+private let alertTitle = "Oh, no! ☹️"
+private let alertMessage = "You need authorize Memories to access your photos to create a cool memory. You may update this permission in your settings."
+private let actionTitle = "Okay"
+
+class MemoryDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var memory : Memory?
+    var memoryController = MemoryController()
     
     @IBOutlet weak var imageView: UIImageView!
     
@@ -16,17 +27,64 @@ class MemoryDetailViewController: UIViewController {
     
     @IBOutlet weak var textView: UITextView!
     
-    
     @IBAction func addPhotoButton(_ sender: Any) {
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        if authorizationStatus == .authorized {
+            presentImagePickerController()
+        } else if authorizationStatus == .notDetermined {
+            PHPhotoLibrary.requestAuthorization { (status) in
+                if authorizationStatus == .authorized {
+                    self.presentImagePickerController()
+                } else if authorizationStatus == .denied {
+                     let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { (action) in
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     @IBAction func savePhotoButton(_ sender: Any) {
+        memoryController.saveToPersistentStore()
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    func updateViews() {
+        guard let memory = memory else {
+            self.title = addMemory
+            return
+        }
+        self.title = editMemory
+        textField.text = memory.title
+        textView.text = memory.bodyText
+        imageView.image = UIImage(data: memory.imageData)
+    }
+    
+    func presentImagePickerController() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            present(picker, animated: true, completion: nil)
+        } else {
+            return
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        imageView.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 
 }
