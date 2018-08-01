93649559235974a80fafb2fe0b8194b74f858e8e
David Doswell <davidoliverdoswell@icloud.com>
2018-08-01 13:34:56 -0700

    set up storyboards with requisite outlets and subclasses

---
+++ Memories/Memories/Controllers/MemoryDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryDetailViewController.swift
+//  Memories
+//
+//  Created by David Oliver Doswell on 8/1/18.
+//  Copyright © 2018 David Oliver Doswell. All rights reserved.
+//
+
+import UIKit
+
+class MemoryDetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    @IBAction func addPhotoButton(_ sender: Any) {
+    }
+    
+    @IBAction func savePhotoButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Memories/Memories/Controllers/OnboardingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingViewController.swift
+//  Memories
+//
+//  Created by David Oliver Doswell on 8/1/18.
+//  Copyright © 2018 David Oliver Doswell. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    @IBAction func getStarted(_ sender: Any) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
