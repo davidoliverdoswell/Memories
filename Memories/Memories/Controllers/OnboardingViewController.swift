0e5fae0ef3dd537551f5c891ca6b2948ce3f54a2
David Doswell <davidoliverdoswell@icloud.com>
2018-08-01 14:43:56 -0700

    implemented logic for OnBoardingVC

--- Memories/Memories/Controllers/OnboardingViewController.swift
+++ Memories/Memories/Controllers/OnboardingViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     
+    let localNotificationHelper = LocalNotificationHelper()
+    
     @IBAction func getStarted(_ sender: Any) {
+        localNotificationHelper.requestAuthorization { (success) in
+            self.localNotificationHelper.scheduleDailyReminderNotification()
+        }
     }
     
-
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "showMaster", sender: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        /* create a conditional statement that will check the value of the status returned in the completion closure of the getAuthorizationStatus function.
+        
+        If the user has already authorized local notifications, then perform the manual segue to send them straight to the table view controller
+ */
+        
+        localNotificationHelper.getAuthorizationStatus { (status) in
+            if status == .denied {
+                NSLog("User denied authorization access")
+            } else if status == .notDetermined {
+                NSLog("User authorization status is undetermined")
+            } else if status == .authorized {
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "showMaster", sender: nil)
+                }
+            }
+        }
     }
 
 }
---
+++ Memories/Memories/Models/LocalNotificationHelper.swift
@@ -0,0 +1,61 @@
+//
+//  LocalNotificationHelper.swift
+//  Memories
+//
+//  Created by David Oliver Doswell on 8/1/18.
+//  Copyright © 2018 David Oliver Doswell. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class LocalNotificationHelper {
+    
+    // helper methods for requesting and getting authorization
+    func getAuthorizationStatus(completion: @escaping (UNAuthorizationStatus) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+            DispatchQueue.main.async {
+                completion(settings.authorizationStatus)
+            }
+        }
+    }
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if let error = error { NSLog("Error requesting authorization status for local notifications: \(error)") }
+            
+            DispatchQueue.main.async {
+                completion(success)
+            }
+        }
+    }
+    
+    private let identifier = "notification"
+    
+    // set up a local notification to fire every day at 8 PM
+    func scheduleDailyReminderNotification() {
+        
+        var date = DateComponents()
+        date.hour = 20
+
+        let content = UNMutableNotificationContent()
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                NSLog("Error scheduling notification \(error)")
+                return
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+}
